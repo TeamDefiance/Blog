6522dd27063578f588ae921010b7eaef71d6c589
diljkap <diljkap@gmail.com>
2016-12-07 11:07:12 +0200

    Edited About Author view

--- src/components/About/AboutForm.js
+++ src/components/About/AboutForm.js
@@ -3,39 +3,24 @@ import React, {Component} from 'react';
 export default class AboutForm extends Component {
     render() {
         return (
-            <div>
-                <div className="form-group">
-                    <label>Name:</label>
-                    <input
-                        className="form-control"
-                        type="text"
-                        name="name"
-                        value={this.props.fullName}
-                        disabled={this.props.submitDisabled}
-
-                    />
+            <div className="author-info" >
+                <div className="author-data">
+                    <label>Name: </label>
+                    <span>
+                        {this.props.fullName}
+                    </span>
                 </div>
-                <div className="form-group">
-                    <label>Email:</label>
-                    <input
-                        className="form-control"
-                        type="text"
-                        name="email"
-                        value={this.props.email}
-                        disabled={this.props.submitDisabled}
-
-                    />
+                <div className="author-data">
+                    <label>Email: </label>
+                    <span >
+                        {this.props.email}
+                    </span>
                 </div>
-                <div className="form-group">
-                    <label>Phone:</label>
-                    <input
-                        className="form-control"
-                        type="text"
-                        name="phone"
-                        value={this.props.phone}
-                        disabled={this.props.submitDisabled}
-
-                    />
+                <div className="author-data">
+                    <label>Phone: </label>
+                    <span >
+                        {this.props.phone}
+                    </span>
                 </div>
 
             </div>
