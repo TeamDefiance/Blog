4584b9eef4f0fc967c72812e16c444abc51ba245
Georgiew3979 <d.georgiew@mail.bg>
2016-12-07 09:35:58 +0200

    add InfoAuthor button

--- src/components/Posts/PostControls.js
+++ src/components/Posts/PostControls.js
@@ -5,15 +5,19 @@ export default class PostControls extends Component {
     render() {
         let edit = null;
         let deletePost = null;
+        let infoAuthor = null;
 
         if (this.props.canEdit) {
             edit = <Link to={"/edit/" + this.props.postId} className="btn btn-default">Edit post</Link>;
             deletePost = <Link to={"/delete/" + this.props.postId} className="btn btn-default">Delete post</Link>;
+        } else{
+            infoAuthor = <Link to={"/about/" + this.props.author} className="btn btn-default">Info for Author</Link>;
         }
         return (
             <div>
                 {edit}
                 {deletePost}
+                {infoAuthor}
             </div>
         )
     }
