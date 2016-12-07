612ce3181f7757321459c065bb4a93d4f83f14da
diljkap <diljkap@gmail.com>
2016-12-07 12:09:26 +0200

    Edited Footer visualization

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -4,10 +4,12 @@ import './Footer.css';
 export default class Footer extends Component {
     render() {
         return (
+            <div>
+                <p id="footer-placeholder"></p>
             <footer className="footer">
                (c)2016 Defiance Team - D&K&D
             </footer>
-
+                </div>
         );
     }
 }
